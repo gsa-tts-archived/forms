ca807619030294ed1c733d9e32fe2a163f0d2b2c
ethangardner <ethan.gardner@gsa.gov>
2025-01-23 11:35:53 -0500

    use fixture for test setup

--- e2e/src/import.spec.ts
+++ e2e/src/import.spec.ts
@@ -1,11 +1,66 @@
-import { test, expect, Page } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 import { GuidedFormCreation, Create } from '../../packages/design/src/FormManager/routes';
 import { BASE_URL } from './constants';
 import { pathToRegexp } from 'path-to-regexp';
 
-const gotoCreatePage = async (page: Page) => {
-  await page.goto(`${BASE_URL}/${GuidedFormCreation.getUrl()}`);
-}
+type FormImportFixture = {
+  formUrl: string;
+};
+
+const test = base.extend<FormImportFixture>({
+  formUrl: async ({ page }, use) => {
+    const { regexp } = pathToRegexp(Create.path);
+    await page.goto(`${BASE_URL}/${GuidedFormCreation.getUrl()}`);
+    await page.getByText('Or use an example file,').click();
+    await page.getByRole('button', { name: 'sample-documents/doj-pardon-' }).click();
+
+    await page.waitForURL((url) => {
+      const pagePath = url.hash.startsWith('#') ? url.hash.substring(1) : url.pathname;
+      return regexp.test(pagePath);
+    });
+
+    // the 'use' is the key for making this fixture available to other tests.
+    const generatedFormUrl = page.url();
+    await use(generatedFormUrl);
+  },
+});
+
+// Start with imported PDF
+// Manipulate page title
+// Format imported text as h2 using rich text editor
+// Move component between pages.
+// Navigate to new page.
+// Delete a component.
+// Reorder pages via drag and drop
+// Add a new page
+// Update title
+// Open dropdown for options and add a package download component - add text to component
+
+test.describe('Import form from a provided sample', () => {
+  test('Verify the form import was successful', async ({ page, formUrl }) => {
+    await page.goto(formUrl);
+    await expect(page.getByText('Blueprint loaded')).toBeVisible();
+  });
+
+  test('Edit page title', async ({ page, formUrl }) => {
+    await page.goto(formUrl);
+    await page.getByText('0 *').click();
+    const input = page.getByLabel('Page title');
+    await input.fill('My awesome form page');
+    await page.getByRole('button', {name: 'Save and Close'}).click();
+    await expect(page.getByRole('link', { name: 'My awesome form page' })).toBeVisible();
+  });
+
+  test('Format description in rich text editor', async ({ page, formUrl }) => {
+    await page.goto(formUrl);
+  });
+
+  // test('Submit generated form', async ({ page, formUrl }) => {
+  //   await page.goto(formUrl);
+  //   await page.getByRole('button', { name: 'Submit' }).click();
+  //   await expect(page.getByText('Submission Successful')).toBeVisible();
+  // });
+});
 
 // const addQuestions = async (page: Page) => {
 //   const menuButton = page.getByRole('button', { name: 'Question', exact: true });
@@ -14,25 +69,25 @@ const gotoCreatePage = async (page: Page) => {
 //   await menuButton.click();
 //   await page.getByRole('button', { name: 'Radio Buttons' }).click();
 // }
-
-test('Import form from sample', async ({ page }) => {
-  const { regexp } = pathToRegexp(Create.path);
-  await page.goto(`${BASE_URL}/${GuidedFormCreation.getUrl()}`);
-  await gotoCreatePage(page);
-  await page.getByText('Or use an example file,').click();
-  await page.getByRole('button', { name: 'sample-documents/doj-pardon-' }).click()
-
-  let pagePath = '';
-  let hasMatch = false;
-  await page.waitForURL((url) => {
-    if (url.hash.startsWith('#')) {
-      pagePath = url.hash.substring(1);
-    } else {
-      pagePath = url.pathname;
-    }
-    hasMatch = regexp.test(pagePath);
-    return hasMatch;
-  });
-
-  expect(hasMatch).toBe(true);
-});
+//
+// test('Import form from sample', async ({ page }) => {
+//   const { regexp } = pathToRegexp(Create.path);
+//   await page.goto(`${BASE_URL}/${GuidedFormCreation.getUrl()}`);
+//   await gotoCreatePage(page);
+//   await page.getByText('Or use an example file,').click();
+//   await page.getByRole('button', { name: 'sample-documents/doj-pardon-' }).click()
+//
+//   let pagePath = '';
+//   let hasMatch = false;
+//   await page.waitForURL((url) => {
+//     if (url.hash.startsWith('#')) {
+//       pagePath = url.hash.substring(1);
+//     } else {
+//       pagePath = url.pathname;
+//     }
+//     hasMatch = regexp.test(pagePath);
+//     return hasMatch;
+//   });
+//
+//   expect(hasMatch).toBe(true);
+// });
