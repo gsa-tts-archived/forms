56f3dbc9ff453d063df842c975fab8c5603c9ac6
ethangardner <ethan.gardner@gsa.gov>
2025-01-24 14:47:52 -0500

    add new test to move component to another page

--- e2e/src/import.spec.ts
+++ e2e/src/import.spec.ts
@@ -1,14 +1,11 @@
 import { test, expect } from './fixtures/import-file.fixture';
 import { enLocale as message } from '@atj/common';
 
-// Format imported text as h2 using rich text editor
-// Move component between pages.
-// Navigate to new page.
 // Delete a component.
 // Reorder pages via drag and drop
 // Add a new page
-// Update title
-// Open dropdown for options and add a package download component - add text to component
+//  - Update title
+//  - Open dropdown for options and add a package download component - add text to component
 
 test.describe('Import form from a provided sample', () => {
   test('Verify the form import was successful', async ({ page, formUrl }) => {
@@ -37,6 +34,23 @@ test.describe('Import form from a provided sample', () => {
     await page.getByRole('button', { name: 'Heading', exact: true }).click();
     await page.getByRole('button', { name: 'Save and Close', exact: true }).click();
     expect(page.getByRole('heading', { name: editorText })).toBeDefined();
+  });
+
+  test('Move question to another page', async ({ page, formUrl }) => {
+    const destinationPage = '1';
+    await page.goto(`${formUrl}?page=2`);
+    await page.getByText('Name:').click();
+    await page.getByRole('button', { name: 'Move questions' }).click();
+    await page.getByLabel('Page:').selectOption(destinationPage);
+    await page.getByLabel('Position:').selectOption('top');
+    await page.getByRole('button', { name: 'Move these questions to another page' }).click();
+    await expect(page.getByText('Name:')).not.toBeVisible();
+    await page.goto(`${formUrl}?page=${destinationPage}`);
+
+    // The moved element gets focus when switching to the new page.
+    // Clearing the focus with and enter keypress allows us to then select it by text.
+    await page.keyboard.press('Enter');
+    await expect(page.getByText('Name:')).toBeVisible();
   });
 });
 
