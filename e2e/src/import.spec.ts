f88c042991fc4cb81cbf68f66a431f823ddd8006
ethangardner <ethan.gardner@gsa.gov>
2025-01-24 16:52:53 -0500

    add package download component to last page

--- e2e/src/import.spec.ts
+++ e2e/src/import.spec.ts
@@ -1,11 +1,7 @@
 import { test, expect } from './fixtures/import-file.fixture';
 import { enLocale as message } from '@atj/common';
 
-// Delete a component.
 // Reorder pages via drag and drop
-// Add a new page
-//  - Update title
-//  - Open dropdown for options and add a package download component - add text to component
 
 test.describe('Import form from a provided sample', () => {
   test('Verify the form import was successful', async ({ page, formUrl }) => {
@@ -67,34 +63,15 @@ test.describe('Import form from a provided sample', () => {
     await page.getByRole('button', { name: 'Delete this pattern' }).click();
     await expect(pattern).not.toBeVisible();
   });
-});
 
-// const addQuestions = async (page: Page) => {
-//   const menuButton = page.getByRole('button', { name: 'Question', exact: true });
-//   await menuButton.click();
-//   await page.getByRole('button', { name: 'Short Answer' }).click();
-//   await menuButton.click();
-//   await page.getByRole('button', { name: 'Radio Buttons' }).click();
-// }
-//
-// test('Import form from sample', async ({ page }) => {
-//   const { regexp } = pathToRegexp(Create.path);
-//   await page.goto(`${BASE_URL}/${GuidedFormCreation.getUrl()}`);
-//   await gotoCreatePage(page);
-//   await page.getByText('Or use an example file,').click();
-//   await page.getByRole('button', { name: 'sample-documents/doj-pardon-' }).click()
-//
-//   let pagePath = '';
-//   let hasMatch = false;
-//   await page.waitForURL((url) => {
-//     if (url.hash.startsWith('#')) {
-//       pagePath = url.hash.substring(1);
-//     } else {
-//       pagePath = url.pathname;
-//     }
-//     hasMatch = regexp.test(pagePath);
-//     return hasMatch;
-//   });
-//
-//   expect(hasMatch).toBe(true);
-// });
+  test('Add a new page with a package download component', async ({ page, formUrl }) => {
+    await page.goto(formUrl);
+    await page.getByRole('button', { name: 'Page' }).click();
+    await page.getByRole('link', { name: 'Untitled Page' }).click();
+    await page.getByRole('button', { name: 'Question', exact: true }).click();
+    await page.getByRole('button', { name: message.patterns.packageDownload.displayName }).click();
+    await page.getByLabel(message.patterns.packageDownload.fieldLabel).fill('Download your documents');
+    await page.getByRole('button', {name: 'Save and Close'}).click();
+    await expect(page.getByRole('button', {name: 'Download PDF'})).toBeVisible();
+  });
+});
