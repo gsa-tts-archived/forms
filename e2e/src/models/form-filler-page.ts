e649563f929c1e0496ed4b6c35d1bac40160346e
ethangardner <ethan.gardner@gsa.gov>
2025-04-02 14:09:32 -0400

    cleanup unused methods in page object model

--- e2e/src/models/form-filler-page.ts
+++ e2e/src/models/form-filler-page.ts
@@ -21,13 +21,4 @@ export class FormFillerPage {
     await backButton.click();
   }
 
-  async getInput(role, matcher) {
-    return this.page.getByRole(role, matcher);
-  }
-
-  async updateInputValue(role, matcher, value) {
-    const input = await this.getInput(role, matcher);
-    await input.first().fill(value);
-  }
-
 }
